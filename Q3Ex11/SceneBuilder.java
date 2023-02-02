f0c4a271d497304f645ff2a71b5adf08d00aafb6
Xeralp-R <80310657+Xeralp-R@users.noreply.github.com>
2023-02-02 08:55:20 +0800

    Created SceneBuilder.java
    New thing, with only bare code: only minimally functional
    
    May need to adapt to Grade in the future: to be done at home

---
+++ Q3Ex11/SceneBuilder.java
@@ -0,0 +1,27 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import Subject.java;
+
+public static class SceneBuilder {
+    public static SceneBuilder() {}
+    
+    public static Screen buildScreen (Subject s) {
+        VBox central_display = new VBox();
+        
+        central_display.setAlignment(pos.CENTER);
+            
+        // important stuff for hbox
+        Button button_left = new Button("", ""); // 2nd argument for graphics
+        button_left.setAlignment(pos.CENTER_LEFT);
+        
+        Button button_right = new Button("", ""); // 2nd argument for graphics
+        button_right.setAlignment(pos.CENTER_RIGHT);
+        
+        HBox overarching = new HBox();
+        
+        Scene scene = new Scene(overarching, 640, 400);
+        return scene;
+    }
+}
