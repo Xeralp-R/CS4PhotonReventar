78b10fa0457ba7e03e18f47570a3f315bfc27514
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2023-02-09 07:54:14 +0800

    Added images, made first working prototype
    Corrected code: not yet connected to primary
    stage, though

--- Q3Ex11/app/src/main/java/q3ex11/SceneBuilder.java
+++ Q3Ex11/app/src/main/java/q3ex11/SceneBuilder.java
@@ -5,21 +5,22 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class SceneBuilder {
     private SceneBuilder() {}
     
-    public static Screen buildScreen (Subject s) {
+    public static Scene buildScreen (Subject subject) {
         VBox central_display = new VBox();
         
-        central_display.setAlignment(pos.CENTER);
+        central_display.setAlignment(Pos.CENTER);
             
         // important stuff for hbox
-        Button button_left = new Button("", ""); // 2nd argument for graphics
-        button_left.setAlignment(pos.CENTER_LEFT);
+        Button button_left = new Button("", new ImageView("arrow_back_ios.png")); // 2nd argument for graphics
+        button_left.setAlignment(Pos.CENTER_LEFT);
         
-        Button button_right = new Button("", ""); // 2nd argument for graphics
-        button_right.setAlignment(pos.CENTER_RIGHT);
+        Button button_right = new Button("", new ImageView("arrow_forward_ios.png")); // 2nd argument for graphics
+        button_right.setAlignment(Pos.CENTER_RIGHT);
         
         HBox overarching = new HBox();
         
