09346e6bf31342ac287c1b613b069a4ed41de8d5
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2023-04-17 19:06:46 +0800

    Added filepath-based subject loading
    Added it in SubjectListLoader.java, in addition to the thingy. To implement functionality elsewhere.

--- Q3Continuing/app/src/main/java/q3continuing/app/subject/SubjectListLoader.java
+++ Q3Continuing/app/src/main/java/q3continuing/app/subject/SubjectListLoader.java
@@ -1,7 +1,12 @@
 package q3continuing.app.subject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SubjectListLoader {
-    public SubjectList loadSampleSubjectList () {
+    public SubjectList loadSampleSubjectList() {
         SubjectList sampleSubjectList = new SubjectList();
 
         sampleSubjectList.registerSubject(new Subject("Math", "math.png", 4, 1.75));
@@ -14,5 +19,24 @@ public class SubjectListLoader {
         sampleSubjectList.registerSubject(new Subject("Research", "research.png", 5, 1.5));
 
         return sampleSubjectList;
+    }
+
+    public SubjectList loadSubjectList(String filepath) throws IOException {
+        SubjectList returner = new SubjectList();
+
+        File subjectFile = new File(filepath);
+        FileReader inter_reader = new FileReader(subjectFile);
+        BufferedReader reader = new BufferedReader(inter_reader);
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] array = line.split(",");
+            returner.registerSubject(new Subject(array[0],
+                    array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3])));
+        }
+
+        reader.close();
+
+        return returner;
     }
 }
